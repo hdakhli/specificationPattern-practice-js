85cdbc6c9cc461c6cf4afc6619e8f14976418378
Hicham Dakhli <hicham.dakhli@axa.fr>
2018-02-08 22:17:36 +0100

    solution to exo01

--- src/js/services/PhoneService.js
+++ src/js/services/PhoneService.js
@@ -23,49 +23,49 @@ export class PhoneService {
      * @return All premium phones
      */
     getAllPremiumPhones() {
-        return this.phones;
+        return this.phones.filter(phone => phone.cost >= 400);
     }
 
     /**
      * @return Only brand Samsung
      */
     getSamsungPhones() {
-        return this.phones;
+        return this.phones.filter(phone => phone.brand === 'SAMSUNG');
     }
 
     /**
      * @return Only premium brand Samsung
      */
     getPremiumSamsungPhones() {
-        return this.phones;
+        return this.phones.filter(phone => phone.brand === 'SAMSUNG' && phone.cost >= 400);
     }
 
     /**
      * @return Either brand Samsung or HTC
      */
     getSamsungAndHTCPhones() {
-        return this.phones;
+        return this.phones.filter(phone => phone.brand === 'SAMSUNG' || phone.brand === 'HTC');
     }
 
     /**
      * @return All premium and brand Samsung or HTC
      */
     getPremiumSamsungAndHTCPhones() {
-        return this.phones;
+        return this.phones.filter(phone => (phone.brand === 'SAMSUNG' || phone.brand === 'HTC') && phone.cost >= 400);
     }
 
     /**
      * @return All but brand Samsung
      */
     getAllExceptSamsungPhones() {
-        return this.phones;
+        return this.phones.filter(phone => phone.brand !== 'SAMSUNG');
     }
 
     /**
      * @return All premium but brand Samsung and htc
      */
     getAllPremiumExceptSamsungAndHTCPhones() {
-        return this.phones;
+        return this.phones.filter(phone => (phone.brand !== 'SAMSUNG' && phone.brand !== 'HTC') && phone.cost >= 400);
     }
 
 }
